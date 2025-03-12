fd25970056b64611167488feacc4fd5075214ff9
cogito30 <cogito30g@gmail.com>
2025-03-12 22:16:44 +0900

    feat: add inputForm

--- to_do_list/index.js
+++ to_do_list/index.js
@@ -1,8 +1,10 @@
 const items  = document.querySelectorAll(".check-circle");
+const createButton = document.querySelector(".to-do-create .circle-button");
+const insertFormPosition = document.querySelector(".insert-form-position");
 
 items.forEach(item => {
     item.addEventListener('click' , (e) => {
-        if (e.target.classList.contains('done')){
+        if (e.target.classList.contains('done')) {
             e.preventDefault();
             item.classList.remove('done');
         } else {
@@ -10,3 +12,15 @@ items.forEach(item => {
         }
     })
 });
+
+createButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('open')) {
+        createButton.classList.remove('open');
+        insertFormPosition.classList.remove('open');
+    } else {
+        createButton.classList.add('open');
+        insertFormPosition.classList.add('open');
+    }
+});
+
+
